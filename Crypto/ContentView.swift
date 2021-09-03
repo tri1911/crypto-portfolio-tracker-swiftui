315123cb50c402a9fe1b218e9ff6c09f83759f57
Tri Ho <minhtri191193@gmail.com>
2021-09-02 18:12:07 -0700

    Initial Commit

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Elliot Ho on 2021-09-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Elliot Ho on 2021-09-02.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
